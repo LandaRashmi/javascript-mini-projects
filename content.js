76eb759b5c1fcedff72bd819e65e652d353f63c4
LandaRashmi <129966632+LandaRashmi@users.noreply.github.com>
2023-12-24 13:05:32 +0530

    Create content.js

---
+++ content.js
@@ -0,0 +1,7 @@
+chrome.runtime.onMessage.addListener(
+  function (request, sender, sendResponse) {
+    if (request.action == 'changeColor') {
+      document.body.style.backgroundColor = 'lightblue';
+    }
+  }
+);
